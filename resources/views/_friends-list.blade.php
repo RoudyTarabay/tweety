7773b9bc681d6617eb1d3cde73ab72774bc0904d
Roudy Tarabay <roudytarabay@gmail.com>
2020-11-01 02:09:37 +0200

    set up backend follow system

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,9 +1,9 @@
-<h1 class="mb-4 font-weight-bold pt-4">Friends</h1>
+<h1 class="mb-4 font-weight-bold pt-4">Following</h1>
 <ul class="list-unstyled">
-    @foreach (range(1,8) as $index)
+    @foreach (auth()->user()->follows as $user)
     <li class="mb-4">
         <div class="d-flex align-items-center">
-            <img src="https://i.pravatar.cc/40" class="rounded-circle mr-2" alt="avatar" /> John Doe
+            <img src="{{$user->avatar}}" class="rounded-circle mr-2" alt="avatar" /> {{$user->name}}
         </div>
     </li>
     @endforeach
